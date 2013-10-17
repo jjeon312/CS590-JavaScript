660ac5cadb9c25075102cefb4d7e8cd66738ea7f
Ishag Alexanian <ishag.alexanian@disney.com>
2013-10-16 19:40:05 -0700

    Centered the plane to the point

--- wissmaps.js
+++ wissmaps.js
@@ -4,19 +4,19 @@ function loadmap() {
 Mapping 101
 
 Coordinates run from 180(right) to -180(left) and 90(top) to -90(bottom).
-This means - right is always greater than left.  And top is always greater than bottom.
+This means - right is always greater than left. And top is always greater than bottom.
 
-Maps are always cut as squares (never rectangles).  Because of this our bounds MUST also be square.
+Maps are always cut as squares (never rectangles). Because of this our bounds MUST also be square.
 Even when we are supplied a rectangle map - it will always, always be cut into a square by the tiling
-program.  Each square tile is always sized 256px.  
+program. Each square tile is always sized 256px.
 
-So we run into an issue.  If we are given a rectangle map - we CANNOT just simple crop it because we
-would loose valuble information (ie. targets, cameras, etc).  So we rely on the tiling system to make
+So we run into an issue. If we are given a rectangle map - we CANNOT just simple crop it because we
+would loose valuble information (ie. targets, cameras, etc). So we rely on the tiling system to make
 a faux square - meaning it will cut the image into a square and fill in extra space with black
 background.
 
 Now we are faced with having to deal with bounds that fit a rectangle, but now must be translated to fit a square.
-So we must do some prep work before we can get into 'real' mapping work.  Basically just like the tiling program
+So we must do some prep work before we can get into 'real' mapping work. Basically just like the tiling program
 we need to create an adjusted faux bound and then recompensate when plotting targets.
 
 Follow the rabbit down the hole to see how this is done...
@@ -63,7 +63,7 @@ var top = 29.1393509;
 
 
    //now lets get some faux centers
-   //plus to confuse matters OpenLayers does LonLat instead of normal LatLon  - this is your first and final warning on that
+   //plus to confuse matters OpenLayers does LonLat instead of normal LatLon - this is your first and final warning on that
    //longitude are those vertical lines - so that means the center point between the two is the left vertical line + the right vertical line/2
    var centerLon = (adjLeft + adjRight) / 2;
    var centerLat = (adjBottom + adjTop) / 2;
@@ -108,21 +108,21 @@ var top = 29.1393509;
    'output/',
    {
        type: 'png',
-       getURL: get_my_url      
+       getURL: get_my_url
    });
 
    /* Dirty Point Plotiung
 
-    Yep - you read that right. Now we need to do some dirty work. Because we went and adjusted
-    our real world bounds.  So to compensate for that offset - because in the real world your bounds
-    are the ones defined at the tip-top of the fucntion - not the adjusted ones.  
+Yep - you read that right. Now we need to do some dirty work. Because we went and adjusted
+our real world bounds. So to compensate for that offset - because in the real world your bounds
+are the ones defined at the tip-top of the fucntion - not the adjusted ones.
 
-    So we have to adjust all our targets so the appear on the map properly.  So what we are going to
-    do is keep the real lat/lon for cool things like popups/hovers but plot dirty points for targets
-so that all are targets appear to be in the correct location.  
-    Basically doing some slight of hand, obtical illusions.
+So we have to adjust all our targets so the appear on the map properly. So what we are going to
+do is keep the real lat/lon for cool things like popups/hovers but plot dirty points for targets
+so that all are targets appear to be in the correct location.
+Basically doing some slight of hand, obtical illusions.
 
-   */
+*/
 
    //first we need to know if we are going to dirty the lon (left/right) or lat (top/bottom)
    //hence why I said keep a clean version - hope you didn't cheat
@@ -156,7 +156,7 @@ so that all are targets appear to be in the correct location.
        [right,top],[left,top],
        [right,bottom],[left,bottom]];
 
-   //All we are doing here is some styling.  What we want is from far away our targets point to appear tiny
+   //All we are doing here is some styling. What we want is from far away our targets point to appear tiny
 //as you zoom in those targets get larger and so do out points
 //this needs to be cleaned up because we hard coded the 2.25 which is just a magical made up number for this instance
 //the size should be dyamic with the zoom there is an OpenLayers example on this sort of
@@ -222,13 +222,13 @@ pointFeatures.push(polygonFeature);
 
 /*
 
-       pointFeature = new OpenLayers.Feature.Vector(OpenLayers.Geometry.Polygon.createRegularPolygon(
-   pointGeometry,
-   0.001,
-   5
+pointFeature = new OpenLayers.Feature.Vector(OpenLayers.Geometry.Polygon.createRegularPolygon(
+pointGeometry,
+0.001,
+5
 ));
 
-       pointFeatures.push(pointFeature);  
+pointFeatures.push(pointFeature);
 */
    }
 
@@ -253,8 +253,14 @@ function returnPolygonFeature(point)
 {
 
 var pointList = [];
-           for(var p=0; p<xPoint.length; ++p) {
-               
+           
+		   var maxX = maxY = minX = minY = 0; 
+		   for(var p=0; p<xPoint.length; ++p) {
+               if ( maxX < xPoint[p] ) maxX = xPoint[p]; 
+			   if ( maxY < yPoint[p] ) maxY = yPoint[p]; 
+			   if ( minX > xPoint[p] ) minX = xPoint[p]; 
+			   if ( minY > yPoint[p] ) maxY = yPoint[p]; 
+			   
                var newPoint = new OpenLayers.Geometry.Point(point.x + xPoint[p],
                                                             point.y + yPoint[p]);
                pointList.push(newPoint);
@@ -262,9 +268,19 @@ var pointList = [];
            pointList.push(pointList[0]);
 
            var linearRing = new OpenLayers.Geometry.LinearRing(pointList);
-linearRing.resize(0.00001, point);
-           var polygonFeature = new OpenLayers.Feature.Vector(
+		   
+		   // Centering the plane to the point 
+		   var moveX = -Math.round((maxX + minX)/2);
+		   var moveY = -Math.round((maxY + minY)/2);
+	       linearRing.move( moveX, moveY );		   
+		   
+		   // Resizing the plane
+		   linearRing.resize(0.00001, point);
+           
+		   
+		   var polygonFeature = new OpenLayers.Feature.Vector(
                new OpenLayers.Geometry.Polygon([linearRing]));
-             return polygonFeature;
+             
+			 return polygonFeature;
 
 }
